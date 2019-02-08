9cdfba8d3b8202ec747ccdf1ed4fe7aae4d9dbce
Hitler <a85504@alunos.uminho.pt>
2019-02-08 14:33:51 +0000

    cenas

--- ficha1.hs
+++ ficha1.hs
@@ -1,13 +1,12 @@
 module Ficha1 where
 
-import Control.Monad
 import Data.Maybe
 
 --1
 --
-cenas1 = (*) 2 . (1 +)
+cenas1 = (*) 2 . (+) 1
 
-cenas2 = succ . (2 *)
+cenas2 = succ . (*) 2
 
 cenas3 :: [a] -> Int
 cenas3 = succ . length
@@ -23,3 +22,24 @@ reverse' = foldl (flip (:)) []
 
 catMaybes' :: [Maybe a] -> [a]
 catMaybes' = map fromJust . filter isJust
+--4
+uncurry' :: (a -> b -> c) -> (a,b) -> c
+uncurry' f (a,b) = f a b
+
+curry' :: ((a,b) -> c) -> a -> b -> c
+curry' f a b = f (a,b)
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f b a = f a b
+
+--7
+concat' :: [[a]] -> [a]
+concat' [[]] = []
+concat' [] = []
+concat' (h:t) = h ++ concat' t
+--9
+m :: (a -> b) -> [a] -> [b]
+m f = foldr ((:) . f) []
+
+m' :: (a -> b) -> [a] -> [b]
+m' = map
